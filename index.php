fcac2150a6775b57f0f4a54db27873ccb58e584d
btRooke <rookebilly@gmail.com>
2021-07-16 18:23:58 +0100

    Upload to real site

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+    <head>
+        <title>Melbourne Place</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body, html {
+                margin: 0;
+                padding: 0;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div style="display: flex; align-items: center; justify-content: center; height: 100vh; flex-direction: column; font-family: sans-serif">
+            <h1>Melbourne Place</h1>
+            <p>Site deployed via GitHub...</p>
+        </div>
+    </body>
+
+</html>
