f7c47316988275a961df46102ad8b1afcae6874a
btRooke <rookebilly@gmail.com>
2021-07-16 19:29:28 +0100

    Update coming soon items

--- index.php
+++ index.php
@@ -15,6 +15,13 @@
         <div style="display: flex; align-items: center; justify-content: center; height: 100vh; flex-direction: column; font-family: sans-serif">
             <h1>Melbourne Place</h1>
             <p>Site deployed via GitHub...</p>
+            <p>Things coming soon:</p>
+            <ul>
+                <li>website-connected doorbell</li>
+                <li>website-connected lights</li>
+                <li>live temperature data</li>
+                <li>party details</li>
+            </ul>
         </div>
     </body>
 
