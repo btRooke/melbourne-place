70513d04d7a31f094b226109fadfa61510759ae0
Billy <rookebilly@gmail.com>
2021-08-04 18:19:06 +0100

    Fix fix fix

--- js/main.js
+++ js/main.js
@@ -18,14 +18,6 @@ bellButton = document.querySelector("#bell");
 
 ringAnimation = () => {
 
-    // hack below to stop mobile hover
-
-    const parent = bellButton.parentNode;
-    const next = bellButton.nextSibling;
-
-    parent.removeChild(bellButton);
-    setTimeout(() => parent.insertBefore(bellButton, next), 0);
-
     bellButton.disabled = true;
     document.querySelector(".overlay").setAttribute("class", "fullScreen centreContent noselect overlay");
 
