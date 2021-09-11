db4d414f8495ab34ac40f6b7a5a9b10448ffd8e2
Sean Patterson <sap22@st-andrews.ac.uk>
2021-09-11 20:21:43 +0100

    Lights almost work now

--- arduino-code/LED/main.py
+++ arduino-code/LED/main.py
@@ -7,21 +7,23 @@ import machine, socket, uasyncio
 # Constants
 PORT = 8080
 
-# Variables
-r = 0
-g = 0
-b = 0
-
 # Main routine
-def main(socket, led):
+def main(socket):
     task = None
+
+    led = [
+        machine.PWM(x, freq=100, duty=0) for x in [
+            machine.Pin(14, machine.Pin.OUT), 
+            machine.Pin(12, machine.Pin.OUT),
+            machine.Pin(13, machine.Pin.OUT)]
+        ]
 
     while True:
         # Accept and decode request
         conn, addr = socket.accept()
         print("Accepted a connection")
 
-        data = conn.recv(2048).decode("utf-8")
+        data = conn.recv(2048).decode("utf-8").split('\n')
         print("Received request from", addr)
         print("Received script:")
         print(data)
@@ -31,31 +33,40 @@ def main(socket, led):
         try:
             for i, line in enumerate(data):
                 data[i], tabs = interpreter.interpret(line, tabs)
-        except:
+        except Exception as e:
             conn.close()
-            fail(led)
+            fail(str(e))
             continue
 
         script = '\n'.join(data)
-        print("Script translated to:\n", script)
+        print("Script translated to:")
+        print(script)
 
         # Run the code in a task
         if task is not None:
             task.cancel()
-        task = uasyncio.create_task(run(conn, script))
+        
+        loop = uasyncio.new_event_loop()
+        task = loop.create_task(exec_async(script, led))
+        loop.run_forever()
         conn.close()
+        print("Checkpoint")
 
-def fail(led):
+# Asynchronously execute a string as a function
+async def exec_async(func, led):
+    print("Executing")
+    exec("async def __script(r, g, b, led):\n{0}".format(func))
+    await locals()['__script'](0, 0, 0, led)
+
+# Set LEDs to red and print error message on failure
+async def fail(msg):
+    print(msg)
+
     led[0].duty(1023)
     led[1].duty(0)
     led[2].duty(0)
 
-async def run(conn, script):
-    try:
-        exec("async def __script():\n{0}".format(script))
-    except Exception as e:
-        conn.send(str(e).encode("utf-8"))
-
+# On executing, start a TCP socket
 if __name__ == "__main__":
     # Listen on port 8080
     addr = socket.getaddrinfo("0.0.0.0", PORT)[0][-1]
@@ -64,12 +75,5 @@ if __name__ == "__main__":
     s.bind(addr)
     s.listen(1) # Only allow 1 unaccepted connection to be queued
     print("Listening on port", PORT)
-
-    # Get LED strip
-    pins = [
-        machine.Pin(14, machine.Pin.OUT), 
-        machine.Pin(12, machine.Pin.OUT),
-        machine.Pin(13, machine.Pin.OUT)]
-
-    pwm = [machine.PWM(x, freq=50, duty=0) for x in pins]   
-    main(s, pwm)
+  
+    main(s)
