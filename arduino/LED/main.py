22b3c7e4754346e46bd5d1cf57cbcd4aacf7e5cb
Sean Patterson <sap22@st-andrews.ac.uk>
2021-12-23 19:20:40 +0000

    Additional LED utils

--- arduino/LED/main.py
+++ arduino/LED/main.py
@@ -1,30 +1,14 @@
 # Local imports
-import interpreter, dimming
+from led import led, lookup, rng
+from parser import generate_tokens, parse
+from setup_net import PORT
 
 # Library imports
-import machine, socket, uasyncio, random
-
-# Constants
-PORT = 8080
-
-task = None
-
-led = [
-    machine.PWM(x, freq=100, duty=0) for x in [
-        machine.Pin(14, machine.Pin.OUT), 
-        machine.Pin(12, machine.Pin.OUT),
-        machine.Pin(13, machine.Pin.OUT)]
-    ]
-
-lookup = [round(dimming.cie1931(float(L) / 255) * 1023) for L in range(256)]
+from uasyncio import get_event_loop, start_server, current_task, CancelledError
 
 # Main routine
-async def main(reader, writer):
+async def main(task, reader, writer):
     try:
-        global task
-        global led
-        global lookup
-
         # Accept and decode request
         print("Accepted a connection")
 
@@ -36,10 +20,13 @@ async def main(reader, writer):
 
         # Translate request into executable python
         script = ""
+        
         try:
-            script = interpreter.interpret(data)
+            tokens = generate_tokens(data)
+            script = parse(tokens)
             print("Script translated to:")
             print(script)
+
         except Exception as e:
             fail(str(e))
             return
@@ -47,12 +34,13 @@ async def main(reader, writer):
         # Run the code in a task, cancelling the previous task
         if task is not None:
             task.cancel()
-        task = uasyncio.current_task()
+
+        task = current_task()
         await exec_async(script, led, lookup)
         task = None
 
     # Catch task getting cancelled by the next request
-    except uasyncio.CancelledError:
+    except CancelledError:
         print("Cancelling current task")
 
     # Close streams on completion
@@ -60,24 +48,29 @@ async def main(reader, writer):
         reader.close()
         writer.close()
 
+
 # Asynchronously execute a string as a function
-async def exec_async(func, led, lookup):
+async def exec_async(func: str, led: list, lookup: list):
     print("Executing")
     exec(func)
-    await locals()['__script'](0, 0, 0, led, lookup)
+    await locals()['__script']({ "r" : 0, "g" : 0, "b" : 0 }, led, lookup, rng)
+
 
 # Set LEDs to red and print error message on failure
-async def fail(msg):
+async def fail(msg: str):
     print(msg)
 
     led[0].duty(1023)
     led[1].duty(0)
     led[2].duty(0)
 
+
 # On executing, start a TCP socket
 if __name__ == "__main__":
-    loop = uasyncio.get_event_loop()
-    loop.create_task(uasyncio.start_server(main, "0.0.0.0", 8080, backlog=1))
+    task = None
+
+    loop = get_event_loop()
+    loop.create_task(start_server(lambda reader, writer: main(task, reader, writer), "0.0.0.0", PORT, backlog=1))
     print("Listening on port", PORT)
     loop.run_forever()
     loop.close()
